cf9f75cfbe05353c5115da3db8e327b76afc2aee
LoganHere <twopaer31@gmail.com>
2026-04-17 17:47:48 +0300

    Создать проект

---
+++ src/main/java/com/loganhere/usercard/UsercardApplication.java
@@ -0,0 +1,13 @@
+package com.loganhere.usercard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loganhere/usercard/UsercardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loganhere.usercard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
